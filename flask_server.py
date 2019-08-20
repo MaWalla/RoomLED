c60543153339688ed6a6b35aed597c47457437b3
Max Walaschewski <mawalla@protonmail.com>
2019-08-20 11:35:08 +0200

    Initial Commit

---
+++ flask_server.py
@@ -0,0 +1,14 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+
+@app.route('/set-led', methods=['POST'])
+def set_led_handler():
+    pass
