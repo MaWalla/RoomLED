b18b86cd83386e3959e58efece7883ba1df8d80d
Max Walaschewski <mawalla99@gmail.com>
2019-10-22 21:30:36 +0200

    Give the main.js an own home

---
+++ js/main.js
@@ -0,0 +1,30 @@
+(function($) {
+    const url = '/led-form';
+    const button = $('#form-submit');
+
+    button.on('click', function (event) {
+        event.preventDefault();
+        button.addClass('btn-warning');
+        button.removeClass('btn-dark');
+        const form = $('#nodemcu-form')[0];
+        fetch(url, {
+            method: 'post',
+            body: new FormData(form),
+        }).then(response => {
+
+            button.removeClass('btn-warning');
+
+            if (response.status === 200) {
+                button.addClass('btn-success');
+            } else {
+                button.addClass('btn-danger');
+            }
+
+            setTimeout(function() {
+                button.addClass('btn-dark');
+                button.removeClass('btn-success');
+                button.removeClass('btn-danger');
+            }, 2000);
+        });
+    });
+})(jQuery);
