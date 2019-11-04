3cd2090532a4758024bebf3875230b2c85b5255d
Max Walaschewski <mawalla99@gmail.com>
2019-11-04 01:09:51 +0100

    Refine navbar

--- roomled/settings.py
+++ roomled/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'oru#t2zukdkf9=^g#1u-c25)&l!hg6mynrzr+1(oby=%dd0dn^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
