d5cfee1c96680a0a7a1c00ee5c52721770d83f5f
Max Walaschewski <mawalla@protonmail.com>
2020-09-10 14:55:48 +0200

    removed hardcoded host, used env variables instead

--- roomled/settings/production.py
+++ roomled/settings/production.py
@@ -10,4 +10,8 @@ if secret_key_env == '${DJANGO_SECRET_KEY}':
 else:
     SECRET_KEY = secret_key_env
 
-ALLOWED_HOSTS = ['mawalla.servebeer.com']
+roomled_host_env = os.path.expandvars('${ROOMLED_HOST}')
+if roomled_host_env == '${ROOMLED_HOST}':
+    raise ImproperlyConfigured('You need to set your host in your environmental variables under ROOMLED_HOST')
+else:
+    ALLOWED_HOSTS = [roomled_host_env]
